7a8e4cace1ca250b568b65b227d1c5e1fa604a69
Nick Sawyer <nick@sawyerdesign.io>
2025-07-23 00:23:58 -0700

    🧹 Cleanup: removed misplaced BRAUN fullcheck scripts

--- scripts/watchers/ghost-fullcheck-alert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-log_in="/Users/sawyer/gitSync/tm-mobile-cursor/.ghost-runner-fullcheck.log"
-alert_out="/Users/sawyer/gitSync/gpt-cursor-runner/.ghost-alerts.log"
-tail -n 20 "$log_in" | grep -E '❌|not running|missing|dead|exit' >> "$alert_out" 
