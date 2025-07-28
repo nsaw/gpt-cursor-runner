1544be3f139384430250cac7e58c0724f2341c19
Nick Sawyer <nick@sawyerdesign.io>
2025-07-28 03:21:11 -0700

    [P6.03] ui-panel-daemon-indicators added with React components

---
+++ dashboard/components/ui/index.ts
@@ -0,0 +1,7 @@
+// Dashboard UI Components
+
+export { default as DaemonStatusPanel } from './DaemonStatusPanel';
+export { default as SystemHealthPanel } from './SystemHealthPanel';
+
+// Re-export hooks for convenience
+export * from '../hooks'; 
