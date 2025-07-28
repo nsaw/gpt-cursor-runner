100433437b75fb2eb798d12a9c81cf5440e9350a
Nick Sawyer <nick@sawyerdesign.io>
2025-07-28 13:27:10 -0700

    [P6.06] ErrorWatcherPanel added with alerts and restart counters

--- dashboard/components/ui/index.ts
+++ dashboard/components/ui/index.ts
@@ -2,6 +2,8 @@
 
 export { default as DaemonStatusPanel } from './DaemonStatusPanel';
 export { default as SystemHealthPanel } from './SystemHealthPanel';
+export { default as LogViewerPanel } from './LogViewerPanel';
+export { ErrorWatcherPanel } from './ErrorWatcherPanel';
 
 // Re-export hooks for convenience
 export * from '../hooks'; 
