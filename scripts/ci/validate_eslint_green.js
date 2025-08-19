35b11ad5670aa8631a106ebde23ca8a1a04513d7
Nick Sawyer <nick@sawyerdesign.io>
2025-08-19 15:09:50 -0700

    guard(nightly): replace inline node-e with file-based validate_eslint_green.js

---
+++ scripts/ci/validate_eslint_green.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+try {
+  const reportPath = path.join(process.env.HOME || process.cwd(), '.cache', 'eslint-report.now.json');
+  const report = JSON.parse(fs.readFileSync(reportPath, 'utf8'));
+  
+  let errorCount = 0;
+  let warningCount = 0;
+  
+  report.forEach(x => {
+    errorCount += x.errorCount || 0;
+    warningCount += x.warningCount || 0;
+  });
+  
+  if (errorCount > 0 || warningCount > 20) {
+    console.error('GREEN-GUARD FAIL', { errors: errorCount, warnings: warningCount });
+    process.exit(1);
+  }
+  
+  console.log('GREEN-GUARD PASS', { errors: errorCount, warnings: warningCount });
+} catch (err) {
+  console.error('GREEN-GUARD ERROR', err.message);
+  process.exit(1);
+}
